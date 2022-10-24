56bae2a18eb1b7751d4bcdbd86bac5fa44011828
Fernando Palafox <fernando.palafox@colorado.edu>
2022-10-24 18:45:35 -0500

    tests for ReverseDiff.jl

--- experiments/demo.jl
+++ experiments/demo.jl
@@ -1,7 +1,6 @@
-# Run this to test code
+# Excercise 18.3 in Nocedal
 
 # Objective function
-# Excercise 18.3 in Nocedal
 function eval_f(x)
     return exp(prod(x)) - 0.5*(x[1]^3 + x[2]^3 + 1)^2 
 end
@@ -20,3 +19,29 @@ end
 x_0 = [-1.71, 1.59, 1.82,-0.763,-0.763]
 
 # Run problem 
+# x_star = sqp_solve(eval_f,eval_c,x_0)
+
+
+## Test stuff
+
+using ReverseDiff: GradientTape, gradient!, compile
+
+# Solution 
+x_star_actual = [-1.8, 1.7, 1.9,-0.8,-0.8]
+
+# Analytical gradient
+function eval_grad_f(x)
+    ∇f = Vector{typeof(x[1])}(undef,5)
+    ∇f[1] = x[2]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[1]^2
+    ∇f[2] = x[1]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[2]^2
+    ∇f[3] = x[1]*x[2]*x[4]*x[5]*exp(prod(x))
+    ∇f[4] = x[1]*x[2]*x[3]*x[5]*exp(prod(x))
+    ∇f[5] = x[1]*x[2]*x[3]*x[4]*exp(prod(x))
+
+    return ∇f
+end
+
+# Test gradient using ReverseDiff.jl
+const f_tape = GradientTape(eval_f,x_0)
+const compiled_f_tape = compile(f_tape)
+
---
+++ src/sqp_solve.jl
@@ -0,0 +1,19 @@
+function sqp_solve
+    # Gradient of objective function 
+
+    # Hessian of Lagrangian 
+
+    # Repeate until convergence conditions 
+        # Evaluate gradient of objective function  
+
+        # Evaluate Hessian
+        
+        # Evaluate Lagrangian
+
+        # Evaluate gradient of constraints
+
+        # Solve quadratic subproblem 
+
+        # Save iterates
+            
+end
