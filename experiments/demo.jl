06fd76599aee8a4db207457d55bc3e780e20c91e
Fernando Palafox <fernando.palafox@colorado.edu>
2022-10-26 17:15:11 -0500

    first version (working)

--- experiments/demo.jl
+++ experiments/demo.jl
@@ -11,37 +11,27 @@ function eval_c(x)
     c[1] = x[1]^2 + x[2]^2 + x[3]^2 + x[4]^2 + x[5]^2 - 10
     c[2] = x[2]*x[3] - 5*x[4]*x[5] 
     c[3] = x[1]^3 + x[2]^3 + 1 
-    print(c)
-    return all(c .== 0)
+    return c
 end
 
 # Starting point
 x_0 = [-1.71, 1.59, 1.82,-0.763,-0.763]
+λ_0 = [1.0,2.0,3.0]
 
 # Run problem 
-# x_star = sqp_solve(eval_f,eval_c,x_0)
-
-
-## Test stuff
-
-using ReverseDiff: GradientTape, gradient!, compile
+x_star = sqp_solve()
 
 # Solution 
-x_star_actual = [-1.8, 1.7, 1.9,-0.8,-0.8]
+x_star_actual = [-1.8, 1.7, 1.9,-0.8,-0.8];
 
-# Analytical gradient
-function eval_grad_f(x)
-    ∇f = Vector{typeof(x[1])}(undef,5)
-    ∇f[1] = x[2]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[1]^2
-    ∇f[2] = x[1]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[2]^2
-    ∇f[3] = x[1]*x[2]*x[4]*x[5]*exp(prod(x))
-    ∇f[4] = x[1]*x[2]*x[3]*x[5]*exp(prod(x))
-    ∇f[5] = x[1]*x[2]*x[3]*x[4]*exp(prod(x))
+# # Analytical gradient
+# function eval_grad_f(x)
+#     ∇f = Vector{typeof(x[1])}(undef,5)
+#     ∇f[1] = x[2]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[1]^2
+#     ∇f[2] = x[1]*x[3]*x[4]*x[5]*exp(prod(x)) - 3*(x[1]^3 + x[2]^3 + 1)*x[2]^2
+#     ∇f[3] = x[1]*x[2]*x[4]*x[5]*exp(prod(x))
+#     ∇f[4] = x[1]*x[2]*x[3]*x[5]*exp(prod(x))
+#     ∇f[5] = x[1]*x[2]*x[3]*x[4]*exp(prod(x))
 
-    return ∇f
-end
-
-# Test gradient using ReverseDiff.jl
-const f_tape = GradientTape(eval_f,x_0)
-const compiled_f_tape = compile(f_tape)
-
+#     return ∇f
+# end
--- src/sqp_solve.jl
+++ src/sqp_solve.jl
@@ -1,19 +1,66 @@
-function sqp_solve
-    # Gradient of objective function 
+using ReverseDiff: GradientTape, JacobianTape, HessianTape, compile, gradient!, jacobian!, hessian!
+using LinearAlgebra: norm
 
-    # Hessian of Lagrangian 
+function sqp_solve()
+    # Set initial iterates
+    x_k = x_0
+    λ_k = λ_0 
+    println("\nx_0 = ", x_0)
 
-    # Repeate until convergence conditions 
+    # Preallocate 
+    length_x     = length(x_0)
+    length_λ     = length(λ_0)
+    grad_f       = similar(x_k)
+    hessian_L    = rand(length_x + length_λ,length_x + length_λ)
+    hessian_L_xx = rand(length_x,length_x)
+    A            = rand(length_λ,length_x)
+    zero_mat     = zeros(typeof(x_0[1]),length_λ,length_λ)
+
+    # Lagrangian
+    function eval_L(v)
+        x = v[1:length_x]
+        λ = v[length_x + 1:end]
+        return eval_f(x) - λ'*eval_c(x)
+    end
+
+    # Setup tapes 
+    tape_f = GradientTape(eval_f,x_0)
+    tape_c = JacobianTape(eval_c,x_0)
+    tape_L = HessianTape(eval_L,rand(length_x + length_λ))
+    tape_f_comp = compile(tape_f)
+    tape_c_comp = compile(tape_c)
+    tape_L_comp = compile(tape_L)
+
+    for i ∈ 1:100
         # Evaluate gradient of objective function  
+        gradient!(grad_f,tape_f_comp,x_k)
 
-        # Evaluate Hessian
-        
-        # Evaluate Lagrangian
+        # Evaluate Hessian of Lagrangian (only at x)
+        hessian!(hessian_L,tape_L_comp,[x_k;λ_k])
+        hessian_L_xx = hessian_L[1:length_x,1:length_x]
 
-        # Evaluate gradient of constraints
+        # Evaluate constraints
+        c = eval_c(x_k)
 
-        # Solve quadratic subproblem 
+        # Evaluate contraint jacobian
+        jacobian!(A,tape_c_comp,x_k)
 
-        # Save iterates
-            
+        # Solve for Newton step 
+        p = inv([hessian_L_xx -A';A zero_mat])*[-grad_f + A'*λ_k; -c]
+        p_x_k = p[1:length_x]
+        p_λ_k = p[length_x+1:end]
+
+        # Set new iterates
+        x_k = x_k + p_x_k
+        λ_k = λ_k + p_λ_k
+
+        # Check merit function 
+        # phi = norm([grad_f - A'*λ_k;eval_c(x_k)])
+
+        # Print
+        println("   p_x_",i," = ", p_x_k)
+        println("   p_λ_",i," = ", p_λ_k)
+        println("x_",i," = ", x_k)
+    end
+    return(x_k)
 end
