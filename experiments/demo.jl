32e4a0078ea2f86a8d01df54ad8aff7f7b1864fb
Fernando Palafox <fernando.palafox@colorado.edu>
2022-10-28 16:05:02 -0500

    cleaned up

--- experiments/demo.jl
+++ experiments/demo.jl
@@ -1,25 +1,16 @@
-# Excercise 18.3 in Nocedal
-
 # Objective function
 function eval_f(x)
-    # return exp(prod(x)) - 0.5*(x[1]^3 + x[2]^3 + 1)^2 
     return x[1]*x[2]^2
 end
 
 # Constraints
 function eval_c(x)
-    # c = Vector{typeof(x[1])}(undef,3)
-    # c[1] = x[1]^2 + x[2]^2 + x[3]^2 + x[4]^2 + x[5]^2 - 10
-    # c[2] = x[2]*x[3] - 5*x[4]*x[5] 
-    # c[3] = x[1]^3 + x[2]^3 + 1 
     c = Vector{typeof(x[1])}(undef,1)
     c[1] = x[1]^2 + x[2]^2 - 2
     return c
 end
 
 # Starting point
-# x_0 = [-1.71, 1.59, 1.82,-0.763,-0.763]
-# λ_0 = zeros(Float64,3)
 x_0 = [-1.5,-1.6]
 λ_0 = zeros(Float64,1)
 
@@ -44,3 +35,25 @@ println("Δx =  ",x_star - x_star_actual)
 
 #     return ∇f
 # end
+
+# # Constraints
+# function eval_c(x)
+#     # c = Vector{typeof(x[1])}(undef,3)
+#     # c[1] = x[1]^2 + x[2]^2 + x[3]^2 + x[4]^2 + x[5]^2 - 10
+#     # c[2] = x[2]*x[3] - 5*x[4]*x[5] 
+#     # c[3] = x[1]^3 + x[2]^3 + 1 
+#     c = Vector{typeof(x[1])}(undef,1)
+#     c[1] = x[1]^2 + x[2]^2 - 2
+#     return c
+# end
+
+# # Objective function
+# function eval_f(x)
+#     # return exp(prod(x)) - 0.5*(x[1]^3 + x[2]^3 + 1)^2 
+#     return x[1]*x[2]^2
+# end
+
+# x_0 = [-1.71, 1.59, 1.82,-0.763,-0.763]
+# λ_0 = zeros(Float64,3)
+
+# Excercise 18.3 in Nocedal
