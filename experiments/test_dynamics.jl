c3b5504420580ed425f00732267d2929eb4c5c88
Fernando Palafox <fernando.palafox@colorado.edu>
2022-11-24 10:19:35 -0600

    fixed indexing

--- experiments/test_dynamics.jl
+++ experiments/test_dynamics.jl
@@ -1,17 +1,21 @@
 # Generate Dubins car trajectory given a control sequence and initial state
 
-V = 1.0
-x_0 = [0.0,0.0,0.0] # Initial state
-u = [1.0,1.0,1.0,1.0,-2.0,-0.5,-0.3] # Control sequence
-Δt = 1.0 # Time step
+x_0 = [0.0,0.0,0.0,0.0] # Initial state
+T = 2 # Number of states after initial state
+u = rand(2*T) # Control sequence
+Δt = 1 # Time step
 
 # First step
-x = Vector{eltype(x_0)}(undef,3*(length(u)+1))
-x[1:3] = x_0
-for i ∈ 1:length(u)
-    x[3*i + 1] = x[3*(i-1)+1] + V*cos(x[3*(i-1)+3])*Δt
-    x[3*i + 2] = x[3*(i-1)+2] + V*sin(x[3*(i-1)+3])*Δt
-    x[3*i + 3] = x[3*(i-1)+3] + u[i]*Δt
+x = Vector{eltype(x_0)}(undef,4*(T+1))
+x[1] = x_0[1] + x_0[4]*cos(x_0[3])*Δt
+x[2] = x_0[2] + x_0[4]*sin(x_0[3])*Δt
+x[3] = x_0[3] + u[1]*Δt
+x[4] = x_0[4] + u[2]*Δt
+for i ∈ 1:T
+    x[4*(i) + 1] = x[4*(i-1)+1] + x[4*(i-1)+4]*cos(x[4*(i-1)+3])*Δt
+    x[4*(i) + 2] = x[4*(i-1)+2] + x[4*(i-1)+4]*sin(x[4*(i-1)+3])*Δt
+    x[4*(i) + 3] = x[4*(i-1)+3] + u[2*(i-1)+1]*Δt
+    x[4*(i) + 4] = x[4*(i-1)+4] + u[2*(i-1)+2]*Δt
 end
 
-z = [x;u]
+z = [x;u];
