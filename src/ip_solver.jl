9726c7ab51bab2e79fb56674ed697d8920a9a098
Fernando Palafox <fernando.palafox@colorado.edu>
2022-12-03 18:53:37 -0600

    first commit

---
+++ src/ip_solver.jl
@@ -0,0 +1,18 @@
+# Interior-point solver 
+
+# Repeat until a stopping test for nonlinear program
+    
+    # Repeat until error function is small enough
+
+        # Solve primal-dual system
+
+        # Compute fraction to the boundary 
+
+        # Update primal and dual variables
+
+        # Update slack variables
+
+        # Update barrier parameter
+
+    # Choose barrier parameter for next iteration
+
